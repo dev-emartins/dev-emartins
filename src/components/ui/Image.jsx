81dfed4e97c775ce5bc158b4a4b4f08101e1b5f0
dev-emartins <dev.everaldomartins@gmail.com>
2026-03-11 19:13:51 -0300

    fix:Repairing the export of the TypeWriter component and the import of Placeholder into the Image component.

--- src/components/ui/Image.jsx
+++ src/components/ui/Image.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
-import Placeholder from "@components/ui/placeholder";
+import Placeholder from "./Placeholder";
 
 function Image({ 
   src, 
