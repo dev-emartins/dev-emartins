713816e087276222bc7189fd64616d70b4e68fd7
Everaldo-Martins <everaldoinfortecnico@gmail.com>
2026-03-16 09:47:47 -0300

    refactor: Refactoring the "about" and "skills" pages.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,19 +15,21 @@ function About(){
             </div>
             <div className="w-full lg:w-2/3 p-3 flex flex-col justify-start items-start gap-3">
                 <h2 className="w-full text-2xl md:text-3xl text-center lg:text-left font-black font-sans">Sobre mim</h2>
-                <h4 className="w-full text-xl font-bold text-center lg:text-left">Olá! Sou Everaldo Martins</h4>
+                <h4 className="w-full text-xl font-bold text-center lg:text-left">Olá! meu nome é Everaldo Martins</h4>
+                
                 <p className="text-base text-justify leading-relaxed">
-                    Sou um desenvolvedor apaixonado por tecnologia em busca da minha primeira oportunidade para adquerir experiência criando soluções web
-                    modernas e eficientes. Desenvolverdor Frontend/Backend, tenho me dedicado a estudar novas técnologias como React, Next.js, Node.js, Spring Boot, Java, Bancos de Dados (Relacionais: PostgreSQL e MySQL e Não-Relacionais: MongoDB) e tecnologias cloud.
+                    Minha paixão pela programação nasceu do desejo de desvendar o funcionamento interno dos sistemas. Desde então, tenho investido no meu crescimento técnico, criando projetos e explorando novas tecnologias para entregar aplicações sólidas, organizadas e fáceis de manter.
                 </p>
                 <p className="text-base text-justify leading-relaxed">
-                    Minha jornada começou com o desejo de entender como as coisas funcionam por trás das telas. Desde então, tenho me aprofundado em
-                    desenvolvimento web, buscando sempre aprender e aplicar as melhores práticas para criar aplicações robustas e escaláveis.
+                    Tenho me dedicado a aprender e usar princípios de design de APIs REST, metodologias de desenvolvimento eficazes, organização de dados e testes, empregando bancos de dados como PostgreSQL, MySQL e MongoDB, assim como ferramentas de conteinerização e tecnologias de nuvem.
                 </p>
                 <p className="text-base text-justify leading-relaxed">
-                    Acredito que a tecnologia deve ser acessível e útil para todos. Por isso, foco em criar interfaces
-                    intuitivas e experiências de usuário excepcionais.
+                    Como desenvolvedor web, almejo a chance de colocar em prática e aprofundar o que aprendi criando soluções inovadoras, de alto desempenho e adaptáveis. Trabalho tanto no lado do servidor quanto na interface (full-stack), com vivência em Java, Spring Boot, Node.js, React e Next.js.
+                </p>                                
+                <p className="text-base text-justify leading-relaxed">
+                    Penso que as melhores soluções tecnológicas unem código de qualidade, performance e usabilidade, e por isso me esforço para criar aplicações com interfaces amigáveis, estrutura coesa e com ênfase em práticas recomendadas de desenvolvimento de software.
                 </p>
+
                 <Download 
                     icon={ FaDownload }
                     to="/files/curriculo.pdf" 
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -61,6 +61,9 @@ function Skills() {
         "JaCoCo",
         "SonarQube",
         "Jira",
+        "MongoDB",
+        "MySQL",
+        "OracleDB",
         "Confluence",            
         "Linux"
     ];
